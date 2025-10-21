52fd0cca7379d6cd8d1dd0236fa99804fee1b036
1Ramez <Ramezmohamed93@gmail.com>
2025-10-21 10:28:22 +0300

    Modified .h files

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <JuceHeader.h>
+
 class PlayerAudio
 {
 public:
     PlayerAudio();
     ~PlayerAudio();
+
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
+
     bool loadFile(const juce::File& file);
     void play();
     void stop();
@@ -15,9 +18,11 @@ public:
     void setPosition(double pos);
     double getPosition() const;
     double getLength() const;
+
 private:
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
+    
     // JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
--- PlayerGUI.h
+++ PlayerGUI.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <JuceHeader.h>
 #include "PlayerAudio.h"
+
 class PlayerGUI : public juce::Component,
     public juce::Button::Listener,
     public juce::Slider::Listener
@@ -8,20 +9,27 @@ class PlayerGUI : public juce::Component,
 public:
     PlayerGUI();
     ~PlayerGUI() override;
+
     void resized() override;
+    void paint(juce::Graphics& g) override;
+
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate);
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill);
     void releaseResources();
 private:
     PlayerAudio playerAudio;
+
     // GUI elements
     juce::TextButton loadButton{ "Load File" };
     juce::TextButton restartButton{ "Restart" };
     juce::TextButton stopButton{ "Stop" };
     juce::Slider volumeSlider;
+
     std::unique_ptr<juce::FileChooser> fileChooser;
+
     // Event handlers
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
 };
